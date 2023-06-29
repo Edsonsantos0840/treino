4fe367cc9ed8c85c7abdd4c13268711abbbc131a
edson <edsonpsantos0840@gmail.com>
2023-06-29 19:43:17 -0300

    feat: (criei componentes de paginas , e o header)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Header from './layout/header'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header/>
      <h1>Olá NextJs</h1>
     </main>
   )
